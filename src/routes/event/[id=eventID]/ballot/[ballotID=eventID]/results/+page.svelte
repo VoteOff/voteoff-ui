ac1fdc3f5b7902700b254a184822801a4de238bc
Jesudas Joseph <jesudasmjoseph@gmail.com>
2025-10-17 13:35:16 -0700

    Cleanup token handling on results page.

--- src/routes/event/[id=eventID]/ballot/[ballotID=eventID]/results/+page.svelte
+++ src/routes/event/[id=eventID]/ballot/[ballotID=eventID]/results/+page.svelte
@@ -1,25 +1,22 @@
 <script lang="ts">
 	import { page } from '$app/state';
 	import { Heading } from 'flowbite-svelte';
-	import { getContext, onMount } from 'svelte';
+	import { getContext } from 'svelte';
 	import type { BallotContext } from '../types';
 	import { voterTokenStorage } from '$lib/token-util';
-	import { EventsAPI } from '$lib/api/events';
 	import ResultWrapper from '$lib/ResultWrapper.svelte';
 
 	const eventID = $derived(Number(page.params.id));
 	const ballotContext: BallotContext = getContext('ballot-data');
-	let token: string = $state('');
-
-	onMount(async () => {
-		const api = new EventsAPI();
-		token = voterTokenStorage.getToken(eventID);
-	});
 </script>
 
 <Heading tag="h2" class="my-8 text-center">Results</Heading>
 <div class="my-4 flex flex-col items-center gap-4">
-	{#if ballotContext.event && token}
-		<ResultWrapper {eventID} votingSystemID={ballotContext.event.electoral_system} {token} />
+	{#if ballotContext.event}
+		<ResultWrapper
+			{eventID}
+			votingSystemID={ballotContext.event.electoral_system}
+			token={voterTokenStorage.getToken(eventID)}
+		/>
 	{/if}
 </div>
