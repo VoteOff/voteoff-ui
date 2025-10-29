0e9dbfa923e78085bba21a12aa38054c5d599fab
Tyler Sheldon <tyler@Kiwi-Machine-II.hsd1.wa.comcast.net>
2025-10-28 23:57:21 -0700

    WIP

--- src/lib/voting-system/components/ranked-choice/RankedChoiceResults.svelte
+++ src/lib/voting-system/components/ranked-choice/RankedChoiceResults.svelte
@@ -1,1 +1,69 @@
-Not Implemented! Ranked Choice Results Component Placeholder
+<script lang="ts">
+	import type { BallotContext, ResultContext } from '$lib/types';
+	import { P } from 'flowbite-svelte';
+	import { getContext } from 'svelte';
+	import type { Submission } from './types';
+
+	const ballotContext: BallotContext = getContext('ballot-data');
+	const resultContext: ResultContext = getContext('results');
+
+	const calculateWinner = (choices, ballots) => {
+	    console.log($state.snapshot(ballots))
+
+	    let winner = null
+
+	    do {
+            const firstChoiceCount = Object.fromEntries(choices.map((choice) => [choice, 0]))
+
+            ballots.forEach((b) => {
+                if (Array.isArray(b.vote) && b.vote) {
+                    firstChoiceCount[b.vote[0]]++
+                }
+            })
+
+            winner = choices[0]
+            let loser = choices[0]
+
+            // Tally up round winner and loser (first choices only)
+
+            choices.forEach((choice) => {
+                if (firstChoiceCount[choice] > firstChoiceCount[winner]) {
+                    winner = choice
+                }
+                if (firstChoiceCount[choice] < firstChoiceCount[loser]) {
+                    loser = choice
+                }
+            })
+
+            console.log(winner, "%%%", loser)
+
+            // Purge loser from the ballots
+
+            ballots = ballots.forEach((b) => {
+                console.log("BE", $state.snapshot(b.vote))
+
+                // tODO something going wrong below
+
+                //if (Array.isArray(b.vote)) {
+                    //b.vote = b.vote.filter((i) => i !== loser)
+                //}
+                //console.log("AF", $state.snapshot(b.vote))
+            })
+
+        } while (choices[winner] / choices.length > .5)
+        //} while (false)
+
+        console.log("winner:", winner)
+
+	}
+
+
+    $effect(() => {
+        calculateWinner(ballotContext.event.choices, resultContext.ballots)
+    })
+
+
+	let votes = $derived(
+		resultContext.ballots
+	);
+</script>
--- src/lib/voting-system/components/ranked-choice/RankedChoiceVoting.svelte
+++ src/lib/voting-system/components/ranked-choice/RankedChoiceVoting.svelte
@@ -1,1 +1,62 @@
-Not Implemented! Ranked Choice Voting Component Placeholder
+<script lang="ts">
+	import { dndzone } from 'svelte-dnd-action';
+	import { flip } from 'svelte/animate';
+	import { getContext } from 'svelte';
+	const flipDurationMs = 200;
+
+	let ballotContext: BallotContext = getContext('ballot-data');
+
+	let items = ballotContext.event.choices.map((choiceStr) => {
+		return { id: choiceStr, title: choiceStr };
+	});
+
+	function handleSort(e) {
+		items = e.detail.items;
+	}
+
+	function handleFinalize(e) {
+	    console.log('handleFinalize()')
+		items = e.detail.items;
+		ballotContext.submission = items.map((i) => i.id);
+	}
+</script>
+
+<p>Drag your preferred options to the top.</p>
+
+<section
+	use:dndzone={{ items, flipDurationMs, dropTargetStyle: { outline: 'none' } }}
+	on:consider={handleSort}
+	on:finalize={handleFinalize}
+>
+	{#each items as item, index (item.id)}
+		<div class="choice-card" animate:flip={{ duration: flipDurationMs }}>
+			<div class="rank-number">
+				{index + 1}.
+			</div>
+			<div>
+				{item.title}
+			</div>
+		</div>
+	{/each}
+</section>
+
+<style>
+	.choice-card {
+		display: flex;
+		min-width: 15em;
+		background-color: #e4e4e4;
+		border-radius: 8px;
+		margin: 15px;
+		padding: 15px 20px;
+	}
+
+	.rank-number {
+		padding-right: 10px;
+		font-weight: bold;
+		color: #909090;
+	}
+
+	section {
+		min-height: 12em;
+	}
+</style>
