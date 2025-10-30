dc98d74ff77bea198ecccb3ac494ea59a544e111
Jesudas Joseph <jesudasmjoseph@gmail.com>
2025-10-29 21:07:52 -0700

    Add feedback for duplicate name

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -2,26 +2,37 @@
 	import type { PageProps } from './$types';
 	import { goto } from '$app/navigation';
 	import { BallotAPI, EventsAPI } from '$lib/api/events';
-	import { Heading, Input, Label, Button, Spinner } from 'flowbite-svelte';
+	import { Heading, Input, Label, Button, Spinner, Alert } from 'flowbite-svelte';
 	import { resolve } from '$app/paths';
 	import { voterTokenStorage } from '$lib/token-util';
 	import { onMount } from 'svelte';
+	import { APIError } from '$lib/api/base';
 
 	const { data }: PageProps = $props();
 
 	let voterName = $state('');
 	let loading: boolean | undefined = $state(undefined);
 
+	let error = $state<string | null>(null);
 	const isFormValid = $derived(voterName.trim() !== '');
 
 	const handleSubmit = async (event: SubmitEvent) => {
 		event.preventDefault();
 
 		const api = new EventsAPI();
-		const response = await api.createBallot(data.id, voterName, data.share_token);
+		let response;
+		try {
+			response = await api.createBallot(data.id, voterName, data.share_token);
+		} catch (e) {
+			if (e instanceof APIError && e.status === 422) {
+				error = 'That name is already taken. Please choose another name.';
+				return;
+			} else {
+				throw e;
+			}
+		}
 
 		voterTokenStorage.saveToken(data.id, response.ballot_token);
-
 		await goto(resolve(`/event/${data.id}/ballot/${response.ballot_id}/vote`));
 	};
 
@@ -50,9 +61,23 @@
 {#if loading === false}
 	<Heading tag="h2" class="my-8 text-center">Register for {data.name}</Heading>
 	<form class="space-y-6" onsubmit={handleSubmit}>
+		{#if error}
+			<Alert color="red">
+				{error}
+			</Alert>
+		{/if}
 		<div>
 			<Label for="name" class="mb-2">Name</Label>
-			<Input id="name" placeholder="Enter your name" bind:value={voterName} required />
+			<Input
+				id="name"
+				placeholder="Enter your name"
+				bind:value={voterName}
+				oninput={() => {
+					console.log('here');
+					error = null;
+				}}
+				required
+			/>
 		</div>
 
 		<div class="flex gap-4">
